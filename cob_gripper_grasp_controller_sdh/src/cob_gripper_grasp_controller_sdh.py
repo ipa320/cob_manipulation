38b909db32874076fb94e58f32108c1a0fbd1b4a
Alexander Bubeck <alexander.bubeck@ipa.fraunhofer.de>
2011-10-07 18:57:49 -0700

    added configuration for pr2-gripper like behaviour of the schunk sdh

--- cob_gripper_grasp_controller_sdh/src/cob_gripper_grasp_controller_sdh.py
+++ cob_gripper_grasp_controller_sdh/src/cob_gripper_grasp_controller_sdh.py
@@ -10,6 +10,9 @@ from simple_script_server import *
 from object_manipulation_msgs.msg import GraspHandPostureExecutionAction
 from object_manipulation_msgs.srv import GraspStatus
 
+fake_pr2_gripper_open = [1.57,-1.57,0.0,-0.50,0.7,-0.50,0.7]
+fake_pr2_gripper_closed = [1.57,-1.57,0.0,-0.20,0.7,-0.20,0.7]
+
 
 class cob_gripper_grasp_controller_sdh:
 
@@ -18,6 +21,7 @@ class cob_gripper_grasp_controller_sdh:
 		self.action_server = actionlib.SimpleActionServer("/sdh_gripper_grasp_posture_controller", GraspHandPostureExecutionAction, self.execute, False)
 		self.query_service = rospy.Service('/sdh_gripper_grasp_status', GraspStatus, self.query_cb)
 		self.action_server.start()
+		time.sleep(1)
 
 	def execute(self, server_goal):
 		if server_goal.goal == 2: #GRASP
@@ -25,14 +29,18 @@ class cob_gripper_grasp_controller_sdh:
 				ros.log_err("Empty grasp given")
 			else:
 				print "moving sdh to grasp ", server_goal.grasp.grasp_posture.position
-				self.sss.move("sdh", server_goal.grasp.grasp_posture.position)
+				#self.sss.move("sdh", server_goal.grasp.grasp_posture.position)
+				self.sss.move("sdh", [fake_pr2_gripper_closed])
+				print "fake_pr2"
 				#self.sss.move("sdh", [[0,0,0,0,0,0,0,0]])
 		if server_goal.goal == 1: #PRE_GRASP
 			if(len(server_goal.grasp.pre_grasp_posture.position) == 0):
 				ros.log_err("Empty pregrasp given")
 			else:
 				print "moving sdh to pregrasp ", server_goal.grasp.pre_grasp_posture.position
-				self.sss.move("sdh", server_goal.grasp.pre_grasp_posture.position)
+				#self.sss.move("sdh", server_goal.grasp.pre_grasp_posture.position)
+				self.sss.move("sdh", [fake_pr2_gripper_open])
+				print "fake_pr2"
 		if server_goal.goal == 3: #RELEASE
 			print "received release command"
 			self.sss.move("sdh", "cylopen")
