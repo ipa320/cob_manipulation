c5c71a8323aea0973eb7981f4eaade7f5821b59c
Alexander Bubeck <alexander.bubeck@ipa.fraunhofer.de>
2011-10-07 17:49:22 -0700

    fixed some naming issue, tested with message from pr2

--- cob_gripper_grasp_controller_sdh/src/cob_gripper_grasp_controller_sdh.py
+++ cob_gripper_grasp_controller_sdh/src/cob_gripper_grasp_controller_sdh.py
@@ -6,9 +6,9 @@ import roslib
 roslib.load_manifest('cob_gripper_grasp_controller_sdh')
 import rospy
 
-from simple_script_server import script
-from object_manipulation_msgs.msgs import GraspHandPostureExecutionAction, GraspStatus
-
+from simple_script_server import *
+from object_manipulation_msgs.msg import GraspHandPostureExecutionAction
+from object_manipulation_msgs.srv import GraspStatus
 
 
 class cob_gripper_grasp_controller_sdh:
@@ -17,22 +17,23 @@ class cob_gripper_grasp_controller_sdh:
 		self.sss = simple_script_server()
 		self.action_server = actionlib.SimpleActionServer("/sdh_gripper_grasp_posture_controller", GraspHandPostureExecutionAction, self.execute, False)
 		self.query_service = rospy.Service('/sdh_gripper_grasp_status', GraspStatus, self.query_cb)
+		self.action_server.start()
 
 	def execute(self, server_goal):
-		if server_goal.goal == GraspHandPostureExecutionAction.GRASP:
+		if server_goal.goal == 2: #GRASP
 			if(len(server_goal.grasp.grasp_posture.position) == 0):
 				ros.log_err("Empty grasp given")
 			else:
 				print "moving sdh to grasp ", server_goal.grasp.grasp_posture.position
 				self.sss.move("sdh", server_goal.grasp.grasp_posture.position)
 				#self.sss.move("sdh", [[0,0,0,0,0,0,0,0]])
-		if server_goal.goal == GraspHandPostureExecutionAction.PRE_GRASP:
+		if server_goal.goal == 1: #PRE_GRASP
 			if(len(server_goal.grasp.pre_grasp_posture.position) == 0):
 				ros.log_err("Empty pregrasp given")
 			else:
 				print "moving sdh to pregrasp ", server_goal.grasp.pre_grasp_posture.position
 				self.sss.move("sdh", server_goal.grasp.pre_grasp_posture.position)
-		if server_goal.goal == GraspHandPostureExecutionAction.RELEASE:
+		if server_goal.goal == 3: #RELEASE
 			print "received release command"
 			self.sss.move("sdh", "cylopen")
 
@@ -43,4 +44,8 @@ class cob_gripper_grasp_controller_sdh:
 
 
 if __name__ == "__main__":
+	rospy.init_node('cob_gripper_grasp_controller_sdh')
 	gripper = cob_gripper_grasp_controller_sdh()
+	print "Running cob_gripper_grasp_controller_sdh"
+	while not rospy.is_shutdown():
+		rospy.sleep(1.0)
