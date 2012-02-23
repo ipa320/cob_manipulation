e85b36487846835cd97f29130b0fb597f3e6c13c
fmw-jk <jannik.kett@googlemail.com>
2012-02-23 13:46:48 +0100

    read bag file and send model msg to moveModel action

--- cob_mmcontroller/src/test_articulationModelAction.py
+++ cob_mmcontroller/src/test_articulationModelAction.py
@@ -4,6 +4,10 @@ import roslib; roslib.load_manifest('cob_mmcontroller')
 import rospy
 import actionlib
 
+import rosbag
+
+import sys
+
 from cob_mmcontroller.msg import *
 from cob_mmcontroller.srv import *
 from articulation_msgs.msg import ParamMsg
@@ -13,25 +17,17 @@ if __name__ == '__main__':
     client = actionlib.SimpleActionClient('moveModel', ArticulationModelAction)
     client.wait_for_server()
 
-    record_srv = rospy.ServiceProxy('/record_track', RecordTrack)
-
-    request = RecordTrackRequest()
-    request.file_name.data = "rotational_y_0.5_"
-    response = record_srv(request)
-
     goal = ArticulationModelGoal()
     # Fill in the goal here
     goal.model_id = 1
-    goal.model.name = "rotational"
-    goal.model.params.append(ParamMsg('angle', 1.57, 1))
-    goal.model.params.append(ParamMsg('rot_center.x', 0.0, 1)) 
-    goal.model.params.append(ParamMsg('rot_center.y', 0.5, 1)) 
-    goal.model.params.append(ParamMsg('rot_center.z', 0.0, 1)) 
+
+    bag = rosbag.Bag(sys.argv[1])
+    for topic, msg, t in bag.read_messages(topics='track'):
+        goal.model = msg
+
+    #print goal.model
     goal.target_duration.data.secs = 10.0
 
     client.send_goal(goal)
     client.wait_for_result(rospy.Duration.from_sec(15.0))
     print client.get_result()
-
-    response = record_srv(request)
-    print response
