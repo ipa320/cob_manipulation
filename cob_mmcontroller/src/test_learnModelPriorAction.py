fcb6e2c0e4e8f74e6b020dc3b537285a0938311b
fmw-jk <jannik.kett@googlemail.com>
2012-03-06 20:39:16 +0100

    test, launch and action for cob_learn_model_prior.py

--- cob_mmcontroller/src/test_learnModelPriorAction.py
+++ cob_mmcontroller/src/test_learnModelPriorAction.py
@@ -6,14 +6,25 @@ import actionlib
 
 from cob_mmcontroller.msg import *
 
-if __name__ == '__main__':
+def main():
     rospy.init_node('learnModelPrior_client')
     client = actionlib.SimpleActionClient('learn_model_prior', LearnModelPriorAction)
     client.wait_for_server()
 
     goal = LearnModelPriorGoal()
     # Fill in the goal here
-    client.send_goal(goal)
-    #print client.get_goal_status_text()
-    client.wait_for_result()
+    client.send_goal(goal, feedback_cb=print_feedback)
+    print client.get_state()
+    while client.get_state() == 0 or client.get_state() == 1:#== actionlib.SimpleGoalState.PENDING or client.get_state() == actionlib.SimpleGoalState.ACTIVE:
+        #print client.get_goal_status_text()
+        client.wait_for_result(rospy.Duration.from_sec(1.0))
+    print "RESULT:"
     print client.get_result()
+
+def print_feedback(feedback):
+    print "Feedback:"
+    print feedback
+
+
+if __name__ == '__main__':
+    main()
