00ecb4006e8dfbb78aba9af9599cf8c0429d5bc6
fmw-jk <jannik.kett@googlemail.com>
2012-03-27 14:03:23 +0200

    bugfix

--- cob_mmcontroller/src/test_moveModelPriorAction.py
+++ cob_mmcontroller/src/test_moveModelPriorAction.py
@@ -11,15 +11,15 @@ from cob_mmcontroller.msg import *
 def main():
     parser = OptionParser()
     parser.add_option('-a', '--action_var', dest = 'action_var', default = 0.0,
-                      action = 'store', help = "describes the movement relative to the start pose 
-                      (e.g. opening angle in rad for a rotational articulation or distance in m 
+                      action = 'store', help = "describes the movement relative to the start pose \
+                      (e.g. opening angle in rad for a rotational articulation or distance in m \
                       for a prismatic one)")
     parser.add_option('-i', '--model_id', dest = 'model_id', default = -1,
                       action = 'store', help = "ID of model according to which movement is executed")
     parser.add_option('-d', '--database', dest = 'database', default = "",
                       action = 'store', help = "file name of database to load prior models from")
     parser.add_option('-t', '--target_duration', dest = 'duration', default = 20.0,
-                      action = 'store', help = "duration in seconds until movement shoulld be finished")
+                      action = 'store', help = "duration in seconds until movement should be finished")
     (options, args) = parser.parse_args()
 
     rospy.init_node('moveModelPrior_client')
@@ -28,7 +28,7 @@ def main():
     print "Server OK"
 
     # set up goal
-    goal = moveModelPriorGoal()
+    goal = MoveModelPriorGoal()
     if options.database == "":
         rospy.errlog("No database is given to load prior models from!")
         rospy.signal_shutdown("Database is missing!")
@@ -36,9 +36,9 @@ def main():
     if options.model_id < 0:
         rospy.errlog("No model ID is given according to which a movement could be executed!")
         rospy.signal_shutdown("Model ID is missing!")
-    goal.model_id = options.model_id
-    goal.action_variable = options.action_var
-    goal.duration = options.duration
+    goal.model_id = int(options.model_id)
+    goal.action_variable = float(options.action_var)
+    goal.duration.secs = float(options.duration)
 
     client.send_goal(goal, feedback_cb=print_feedback)
     print client.get_state()
