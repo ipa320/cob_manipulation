9429ce272bcbddc9af9e7a24a5c2d0f3f9289582
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2012-04-10 17:49:40 +0200

    updated IK fast kinematics plugin

--- cob_manipulator/ros/src/ikfast_kinematics_plugin.cpp
+++ cob_manipulator/ros/src/ikfast_kinematics_plugin.cpp
@@ -1,14 +1,11 @@
 
 #include "ros/ros.h"
 #include <kinematics_base/kinematics_base.h>
-#include <kdl/tree.hpp>
 #include <urdf/model.h>
-#include <kdl_parser/kdl_parser.hpp>
-#include <kdl/chainfksolverpos_recursive.hpp>
 #include <tf/tf.h>
 #include <tf_conversions/tf_kdl.h>
-#include <map>
 #include <iostream>
+#include <algorithm>
 #define IKFAST_NO_MAIN
 #ifdef IKFAST_REAL
 typedef IKFAST_REAL IKReal;
@@ -44,28 +41,52 @@ public:
       solution.clear();
       solution.resize(numJoints);
       solutions[i].GetSolution(&solution[0],vsolfree.size()>0?&vsolfree[0]:NULL);
-      std::cout << "solution " << i << ":" ;
-      for(int j=0;j<numJoints; ++j)
-	  std::cout << " " << solution[j];
-      std::cout << std::endl;
+      //std::cout << "solution " << i << ":" ;
+      //for(int j=0;j<numJoints; ++j)
+      //	  std::cout << " " << solution[j];
+      //std::cout << std::endl;
 	  
       //ROS_ERROR("%f %d",solution[2],vsolfree.size());
+    }
+    double harmonize(const std::vector<double> &ik_seed_state, std::vector<double> &solution){
+    	double dist_sqr = 0;
+    	for(size_t i=0; i< ik_seed_state.size(); ++i){
+			if(fabs(solution[i] - ik_seed_state[i]) > 2*M_PI){ 
+				if(ik_seed_state[i] < 0){
+					if(solution[i] > 0)	 solution[i] -= 2*M_PI;
+				}else{
+					if(solution[i] < 0) solution[i] += 2*M_PI;
+				}
+			}
+
+			dist_sqr += fabs(solution[i] - ik_seed_state[i]);
+    	}
+    	return dist_sqr;
+    }
+
+    double harmonize_old(const std::vector<double> &ik_seed_state, std::vector<double> &solution){
+    	double dist_sqr = 0;
+    	for(size_t i=0; i< ik_seed_state.size(); ++i){
+    		double diff = ik_seed_state[i] - solution[i];
+    		if( diff > M_PI ) solution[i]+=2*M_PI; 
+    		else if (diff < -M_PI) solution[i]-=2*M_PI;
+			diff = ik_seed_state[i] - solution[i];
+			dist_sqr += fabs(diff);
+    	}
+    	return dist_sqr;
     }
     virtual void getClosestSolution(const std::vector<double> &ik_seed_state, std::vector<double> &solution){
       double mindist = 0;
       int minindex = -1;
       std::vector<double> sol;
       for(size_t i=0;i<solutions.size();++i){
-	  getSolution(i,sol);
-	  double dist = 0;
-	  for(int j=0;j< numJoints;++j){
-	    double diff = ik_seed_state[j] - sol[j];
-	    dist += diff*diff;
-	  }
-	  if(minindex == -1 || dist<mindist){
-	    minindex = i;
-	    mindist = dist;
-	  }
+		  getSolution(i,sol);
+		  double dist = harmonize(ik_seed_state, sol);
+		  //std::cout << "dist[" << i << "]= " << dist << std::endl;
+		  if(minindex == -1 || dist<mindist){
+			minindex = i;
+			mindist = dist;
+		  }
       }
       if(minindex >= 0) getSolution(minindex,solution);
     }
@@ -78,9 +99,14 @@ private:
     int numJoints;
 };
 
-//Namespace for COB3_2
+//Namespace for cob3-2
 namespace cob3_2{
 #include "ikfast_cob3_2.cpp"
+}
+
+//Namespace for cob3-3
+namespace cob3_3{
+#include "ikfast_cob3_3.cpp"
 }
 
 //Namespace for lwa_sdh
@@ -94,13 +120,11 @@ class IKFastKinematicsPlugin : public kinematics::KinematicsBase
   std::vector<double> jointMin;
   std::vector<double> jointMax;
   std::vector<std::string> links;
-  std::map<std::string,size_t> link2index;
   std::string tip_name, root_name;
-  KDL::Chain kdl_chain;
-  boost::shared_ptr<KDL::ChainFkSolverPos_recursive> jnt_to_pose_solver;
   ik_solver_base* ik_solver;
   size_t numJoints;
   std::vector<int> freeParams;
+  void (*fk)(const IKReal* j, IKReal* eetrans, IKReal* eerot);
   
 public:
 
@@ -120,14 +144,22 @@ public:
 	fillFreeParams(cob3_2::getNumFreeParameters(),cob3_2::getFreeParameters());
 	numJoints = cob3_2::getNumJoints();
 	ik_solver = new ikfast_solver<cob3_2::IKSolution>(cob3_2::ik, numJoints);
-      }if(robot == "lwa_sdh"){
+	fk=cob3_2::fk;
+      }else if(robot == "cob3-3"){
+	fillFreeParams(cob3_3::getNumFreeParameters(),cob3_3::getFreeParameters());
+	numJoints = cob3_3::getNumJoints();
+	ik_solver = new ikfast_solver<cob3_3::IKSolution>(cob3_3::ik, numJoints);
+	fk=cob3_3::fk;
+      }else if(robot == "lwa_sdh"){
 	fillFreeParams(lwa_ext::getNumFreeParameters(),lwa_ext::getFreeParameters());
 	numJoints = lwa_ext::getNumJoints();
 	ik_solver = new ikfast_solver<lwa_ext::IKSolution>(lwa_ext::ik, numJoints);
+	fk=lwa_ext::fk;
       }else{
 	ROS_FATAL("Robot '%s' unknown!",robot.c_str());
 	return false;
       }
+      ROS_INFO("Robot is '%s'!",robot.c_str());
       
       if(freeParams.size()>1){
 	ROS_FATAL("Only one free joint paramter supported!");
@@ -158,43 +190,41 @@ public:
 	return false;
       }
       
-      KDL::Tree tree;
-      if (!kdl_parser::treeFromString(xml_string, tree))
-      {
-	ROS_ERROR("Could not initialize tree object");
-	return false;
-      }
-      
-/*      const KDL::SegmentMap & segments = tree.getSegments ();
-      for( KDL::SegmentMap::const_iterator it =segments.begin();it!=segments.end();++it){
-	ROS_ERROR("Segment %s",it->second.segment.getName().c_str());
-      }*/
-      
-      if (!tree.getChain(root_name, tip_name, kdl_chain))
-      {
-	ROS_ERROR("Could not initialize kdl_chain object");
-	return false;
-      }
-      
-      int i=0; // segment number
-      while(i < (int)kdl_chain.getNrOfSegments())
-      {
-	link2index.insert(std::make_pair(kdl_chain.getSegment(i).getName(),i));
-	links.push_back(kdl_chain.getSegment(i).getName());
-	if(links.size()>1){
-	  joints.push_back(kdl_chain.getSegment(i).getJoint().getName());
-	  ROS_ERROR("Joint %s",joints.back().c_str());
+      node_handle.param(full_urdf_xml,xml_string,std::string());
+      robot_model.initString(xml_string);
 
-	}
-	i++;
+      boost::shared_ptr<urdf::Link> link = boost::const_pointer_cast<urdf::Link>(robot_model.getLink(tip_name));
+      while(link->name != root_name && joints.size() <= numJoints){
+	//ROS_ERROR("link %s",link->name.c_str());
+	links.push_back(link->name);
+	boost::shared_ptr<urdf::Joint> joint = link->parent_joint;
+	if(joint && joint->type != urdf::Joint::FIXED){
+	  //ROS_ERROR("joint %s",joint->name.c_str());
+	  joints.push_back(joint->name);
+	  if(joint->limits){
+	    jointMin.push_back(joint->limits->lower);
+	    jointMax.push_back(joint->limits->upper);
+	  }
+	}else{
+	  ROS_WARN("no joint corresponding to %s",link->name.c_str());
+   	}
+	link = link->getParent();
       }
+      
       if(joints.size() != numJoints){
-	ROS_ERROR("Joints count mismatch");
+	ROS_FATAL("Joints number mismatch.");
 	return false;
       }
       
-      jnt_to_pose_solver.reset(new KDL::ChainFkSolverPos_recursive(kdl_chain));
+      links.push_back(root_name);
       
+      std::reverse(links.begin(),links.end());
+      std::reverse(joints.begin(),joints.end());
+      std::reverse(jointMin.begin(),jointMin.end());
+      std::reverse(jointMax.begin(),jointMax.end());
+      
+      for(size_t i=0; i <numJoints; ++i)
+	ROS_INFO("%s %f %f",joints[i].c_str(),jointMin[i],jointMax[i]);
 	
      return true;
     }
@@ -203,7 +233,7 @@ public:
                        const std::vector<double> &ik_seed_state,
                        std::vector<double> &solution,
 		       int &error_code) {
-	ROS_ERROR("getPositionIK");
+	//ROS_ERROR("getPositionIK");
 	
 	std::vector<double> vfree(freeParams.size());
 	for(std::size_t i = 0; i < freeParams.size(); ++i){
@@ -220,7 +250,7 @@ public:
 		
 	if(numsol){
 	  ik_solver->getClosestSolution(ik_seed_state,solution);
-	  ROS_ERROR("Solved!");
+	  //ROS_ERROR("Solved!");
 	  error_code = kinematics::SUCCESS;
 	  return true;
 	}
@@ -234,7 +264,7 @@ public:
                           std::vector<double> &solution,
 			  int &error_code) {
   
-	ROS_ERROR("searchPositionIK1");
+	//ROS_ERROR("searchPositionIK1");
 	if(freeParams.size()==0){
 	  return getPositionIK(ik_pose, ik_seed_state,solution, error_code);
 	}
@@ -248,28 +278,31 @@ public:
 	const double increment = 0.01; //TODO: make it a plugin parameter
 	int counter = 0;
 
-	while (ros::Time::now() < maxTime){
-	    vfree[0] = ik_seed_state[freeParams[0]] + increment*counter;
+	vfree[0] = ik_seed_state[freeParams[0]];
 
+	do{
 	    int numsol = ik_solver->solve(frame,vfree);
+	    //ROS_INFO("%f",vfree[0]);
 	    
 	    if(numsol > 0){
 		ik_solver->getClosestSolution(ik_seed_state,solution);
-		ROS_ERROR("Solved!");
+		//ROS_ERROR("Solved!");
 		error_code = kinematics::SUCCESS;
 		return true;
 	    }
 	    int i = 0;
 	    do{
 	      if( i >= 2){
-		error_code = kinematics::NO_IK_SOLUTION; 
-		return false;
+			error_code = kinematics::NO_IK_SOLUTION; 
+			return false;
 	      }
-	      counter = counter > 0? - counter: counter + 1;
+	      counter = counter > 0? - counter: 1 - counter;
+	      vfree[0] = ik_seed_state[freeParams[0]] + increment*counter;
+	      //std::cout << vfree[0] << " " << counter << " " << i << "  "  << jointMin[freeParams[0]] << " " << jointMax[freeParams[0]] << std::endl;
 	      ++i;
-	    }while( counter *  increment < jointMin[freeParams[0]] ||  counter *  increment > jointMax[freeParams[0]] );
-	     
-	}
+	    }while( vfree[0] < jointMin[freeParams[0]] ||  vfree[0] > jointMax[freeParams[0]] );
+	    
+	}while (ros::Time::now() < maxTime && vfree[0] >= jointMin[freeParams[0]] &&  vfree[0] <= jointMax[freeParams[0]]);
 	error_code = kinematics::NO_IK_SOLUTION; 
  	return false;
     }      
@@ -281,7 +314,7 @@ public:
                           const boost::function<void(const geometry_msgs::Pose &ik_pose,const std::vector<double> &ik_solution,int &error_code)> &solution_callback,
 			  int &error_code){
       
-	ROS_ERROR("searchPositionIK2");
+	//ROS_ERROR("searchPositionIK2");
 	if(freeParams.size()==0){ // TODO: not closest, but valid solution!
 	  return getPositionIK(ik_pose, ik_seed_state,solution, error_code);
 	}
@@ -303,92 +336,73 @@ public:
 	int counter = 0;
 
 	std::vector<double> sol;
-	while (ros::Time::now() < maxTime){
-	    if(freeParams.size())
-		vfree[0] = ik_seed_state[freeParams[0]] + increment*counter;
-
+	vfree[0] = ik_seed_state[freeParams[0]];
+	do{
 	    int numsol = ik_solver->solve(frame,vfree);
-	    ROS_ERROR("Numsol: %i", numsol); //***DEBUG***
-	    ROS_ERROR("I. Mark"); //***DEBUG***
+	    
 	    if(numsol > 0){
-		ROS_ERROR("Ist solution_callback == empty: %b", solution_callback.empty()); //***DEBUG***
 		if(solution_callback.empty()){
-		    ROS_ERROR("Nun wird die ClosestSolution gesucht"); //***DEBUG***
 		    ik_solver->getClosestSolution(ik_seed_state,solution);
-		    ROS_ERROR("II. Mark"); //***DEBUG***
 		    error_code = kinematics::SUCCESS;
 		    return true;
-		    ROS_ERROR("III. Mark"); //***DEBUG***
 		}
 
 		for (int s = 0; s < numsol; ++s){
-		    ROS_ERROR("s. Mark"); //***DEBUG***
 		    ik_solver->getSolution(s,sol);
 		    solution_callback(ik_pose,sol,error_code);
 		    
 		    if(error_code == kinematics::SUCCESS){
-		      ROS_ERROR("IV. Mark"); //***DEBUG***
 		      solution = sol;
-		      ROS_ERROR("Solved!");
 		      return true;
 		    }
 		}
 	    }
 	    int i = 0;
 	    do{
-	      if( i >= 2 || freeParams.size()==0){
+	      if( i >= 2){
 		error_code = kinematics::NO_IK_SOLUTION; 
 		return false;
 	      }
-	      counter = counter > 0? - counter: counter + 1 ;
+	      counter = counter > 0? - counter: 1 - counter;
+	      vfree[0] = ik_seed_state[freeParams[0]] + increment*counter;
 	      ++i;
-	    }while( counter *  increment < jointMin[freeParams[0]] ||  counter *  increment > jointMax[freeParams[0]] );
-	     
-	}
+	    }while( vfree[0] < jointMin[freeParams[0]] ||  vfree[0] > jointMax[freeParams[0]] );
+	    
+	}while (ros::Time::now() < maxTime && vfree[0] >= jointMin[freeParams[0]] &&  vfree[0] <= jointMax[freeParams[0]]);
+
 	error_code = kinematics::NO_IK_SOLUTION; 
  	return false;
    }      
     bool getPositionFK(const std::vector<std::string> &link_names,
                        const std::vector<double> &joint_angles, 
                        std::vector<geometry_msgs::Pose> &poses){
-	ROS_ERROR("getPositionFK"); //Just a mark for entering PositionFK, NO ERROR!!
+	//ROS_ERROR("getPositionFK"); //Just a mark for entering PositionFK, NO ERROR!!
 	KDL::Frame p_out;
-	KDL::JntArray jnt_pos_in;
-	geometry_msgs::PoseStamped pose;
-	tf::Stamped<tf::Pose> tf_pose;
 
-	jnt_pos_in.resize(joints.size());
-	for(size_t i=0; i < joints.size(); i++)
-	{
-	  jnt_pos_in(i) = joint_angles[i];
+	if(link_names.size()!=1 || link_names[0]!=tip_name){
+	    ROS_ERROR("Can compute FK for %s only",tip_name.c_str());
+	    return false;
 	}
-
-	poses.resize(link_names.size());
-
+	
 	bool valid = true;
-	for(unsigned int i=0; i < poses.size(); i++)
-	{
-	  if(jnt_to_pose_solver->JntToCart(jnt_pos_in,p_out,link2index[link_names[i]]) >=0)
-	  {
-	    tf::PoseKDLToMsg(p_out,poses[i]);
-	  }
-	  else
-	  {
-	    ROS_ERROR("Could not compute FK for %s",link_names[i].c_str());
-	    valid = false;
-	  }
-	}
-  ROS_ERROR("getPositionFK %s",valid?"succeded":"failed");
+	
+	double eerot[9], eetrans[3];
+	fk(&joint_angles[0],eetrans,eerot);
+	for(int i=0; i<3;++i) p_out.p.data[i] = eetrans[i];
+	for(int i=0; i<9;++i) p_out.M.data[i] = eerot[i];
+	
+	poses.resize(1);
+	tf::PoseKDLToMsg(p_out,poses[0]);	
 	
 	return valid;
     }      
-    std::string getBaseFrame()  { 	 ROS_ERROR("getBaseFrame");
+    std::string getBaseFrame()  {// 	 ROS_ERROR("getBaseFrame");
 return root_name; }
-    std::string getToolFrame() { 	 ROS_ERROR("getToolFrame");
+    std::string getToolFrame() { //	 ROS_ERROR("getToolFrame");
 return tip_name; }
-    std::vector<std::string> getJointNames() { 	 ROS_ERROR("getJointNames");
+    std::vector<std::string> getJointNames() { //	 ROS_ERROR("getJointNames");
 return joints; }
-    std::vector<std::string> getLinkNames() { 	 ROS_ERROR("getLinkNames");
+    std::vector<std::string> getLinkNames() { //	 ROS_ERROR("getLinkNames");
 return links; }
 };
 }
