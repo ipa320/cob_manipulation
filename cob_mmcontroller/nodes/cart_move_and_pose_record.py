db42b57f0c440fd5439ec4eeb1932d5c4a8e3a3f
fmw-jk <jannik.kett@googlemail.com>
2012-04-16 19:24:17 +0200

    first running version

--- cob_mmcontroller/nodes/cart_move_and_pose_record.py
+++ cob_mmcontroller/nodes/cart_move_and_pose_record.py
@@ -4,9 +4,8 @@ import roslib; roslib.load_manifest('cob_mmcontroller')
 import rospy
 import actionlib
 
-import PyKDL
-
 from geometry_msgs.msg import *
+from cob_mmcontroller.msg import *
 
 
 class cart_move_and_pose_record:
@@ -19,7 +18,7 @@ class cart_move_and_pose_record:
         self.cart_state_sub = rospy.Subscriber('/arm_controller/cart_state', PoseStamped, self.cartStateCB)
 
         # action server
-        self.moveAndRecord_as = actionlib.SimpleActionServer('moveAndRecord', MoveAndRecordAction, self.moveAndRecordActionCB, False)
+        self.moveAndRecord_as = actionlib.SimpleActionServer('move_and_record', MoveAndRecordAction, self.moveAndRecordActionCB, False)
         self.moveAndRecord_as.start()
 
     def cartStateCB(self, pose):
@@ -28,33 +27,35 @@ class cart_move_and_pose_record:
 
     def moveAndRecordActionCB(self, goal):
         # set up and initialize action feedback and result
-        result_ = MoveModelPriorResult()
-        feedback_ = MoveModelPriorFeedback()
+        result_ = MoveAndRecordResult()
+        feedback_ = MoveAndRecordFeedback()
         result_.success = False
-        feedback_.message = 
+        feedback_.message = "started"
         self.moveAndRecord_as.publish_feedback(feedback_)
 
         # set up 
-        start_time = rospy.get_time()
-        duration = rospy.get_time() - start_time
+        start_time = rospy.get_rostime()
+        duration = rospy.get_rostime() - start_time
 
         # command cartesian twist
         self.cart_command_pub.publish(goal.twist)
 
         # record poses
-        current_pose_stamped = PoseStamped()
+        current_pose = Pose()
+        current_time = rospy.Time()
         while duration <= goal.target_duration:
-            current_pose_stamped.stamp = rospy.Time(duration.to_sec())
-            current_pose_stamped.pose = self.current_pose
-            result_.poses.append(current_pose_stamped)
+            current_time = rospy.Time(duration.to_sec())
+            current_pose = self.current_pose
+            result_.time.append(current_time)
+            result_.poses.append(current_pose)
             rospy.sleep(0.01)
-            duration = rospy.get_time() - start_time
+            duration = rospy.get_rostime() - start_time
 
         # command zero twist
         self.cart_command_pub.publish(Twist())
 
         result_.success = True
-        seld.moveAndRecord_as.set_succeeded(result_)
+        self.moveAndRecord_as.set_succeeded(result_)
 
 
 
@@ -65,5 +66,5 @@ if __name__ == "__main__":
         rospy.init_node('cart_move_and_pose_record')
         cart_move_and_pose_record()
         rospy.spin()
-    except rospy.ROSInterrupedException as e: rospy.signal_shutdown(str(e))
+    except rospy.ROSInterruptException as e: rospy.signal_shutdown(str(e))
                                                                              
--- cob_mmcontroller/src/record_system_behavior.py
+++ cob_mmcontroller/src/record_system_behavior.py
@@ -14,149 +14,158 @@ import PyKDL
 import matplotlib.pyplot as plt
 
 twist_dur_list = [#[0., 0., 0., 0., 0., 0., 0.],
-              [.1, 0., 0., 0., 0., 0., 5.],
-              [-.1, 0., 0., 0., 0., 0., 5.],
-              """[0., .1, 0., 0., 0., 0., 5.],
-              [0., -.1, 0., 0., 0., 0., 5.],
-              [0., 0., .1, 0., 0., 0., 5.],
-              [0., 0., -.1, 0., 0., 0., 5.],
-              [.05, .05, 0., 0., 0., 0., 5.],
-              [-.05, -.05, 0., 0., 0., 0., 5.],
-              [.05, 0., .05, 0., 0., 0., 5.],
-              [-.05, 0., -.05, 0., 0., 0., 5.],
-              [.0, .05, .05, 0., 0., 0., 5.],
-              [.0, -.05, -.05, 0., 0., 0., 5.],
-              [0., 0., 0., .1, 0., 0., 5.], #~5°
-              [0., 0., 0., -.1, 0., 0., 5.], #~5°
-              [0., 0., 0., 0., .1, 0., 5.],
-              [0., 0., 0., 0., -.1, 0., 5.],
-              [0., 0., 0., 0., 0., .1, 5.],
-              [0., 0., 0., 0., 0., -.1, 5.],"""
-              ]
+                 [.1, 0., 0., 0., 0., 0., 5.],
+                 [-.1, 0., 0., 0., 0., 0., 5.]#,
+                 #[0., .1, 0., 0., 0., 0., 5.],
+                 #[0., -.1, 0., 0., 0., 0., 5.],
+                 #[0., 0., .1, 0., 0., 0., 5.],
+                 #[0., 0., -.1, 0., 0., 0., 5.],
+                 #[.05, .05, 0., 0., 0., 0., 5.],
+                 #[-.05, -.05, 0., 0., 0., 0., 5.],
+                 #[.05, 0., .05, 0., 0., 0., 5.],
+                 #[-.05, 0., -.05, 0., 0., 0., 5.],
+                 #[.0, .05, .05, 0., 0., 0., 5.],
+                 #[.0, -.05, -.05, 0., 0., 0., 5.],
+                 #[0., 0., 0., .1, 0., 0., 5.], #~5 degrees
+                 #[0., 0., 0., -.1, 0., 0., 5.], #~5 degrees
+                 #[0., 0., 0., 0., .1, 0., 5.],
+                 #[0., 0., 0., 0., -.1, 0., 5.],
+                 #[0., 0., 0., 0., 0., .1, 5.],
+                 #[0., 0., 0., 0., 0., -.1, 5.]
+                 ]
 
-class move_and_record_client:
-    def __init__(self):
-        client = actionlib.SimpleActionClient('move_and_record', MoveAndRecordAction)
-        client.wait_for_server()
-        print "Server OK"
+rootdir = ''
 
-        self.rootdir = os.path.join("..", data, sys.argv[1])
-        os.mkdir(self.rootdir)
+def main():
+    global rootdir
+    try:
+        rospy.init_node('moveAndRecord_client')
+    except rospy.ROSInterruptException as e: rospy.signal_shutdown(str(e)); sys.exit()
+    
+    client = actionlib.SimpleActionClient('move_and_record', MoveAndRecordAction)
+    client.wait_for_server()
+    print "Server OK"
+    print os.path.abspath(".") 
+    print sys.path[0]
 
-    def record_and_save(self)
-        for twist in twist_dur_list:
-            goal = self.parse_twist_list
+    rootdir = os.path.join(sys.path[0], "..", "data", sys.argv[1])
+    os.mkdir(rootdir)
+    print "created dir"
 
-            client.send_goal(goal, feedback_cb=print_feedback)
-            while client.get_state() == 0 or client.get_state() == 1:
-                client.wait_for_result(rospy.Duration.from_sec(1.0))
+    for twist in twist_dur_list:
+        goal = parse_twist_list(twist)
+        print "move", goal
 
-            result = client.get_result()
-            data_set = self.parse_measurement(result, goal.twist)
-            self.plot_and_save_data_set(goal.twist, data_set)
+        client.send_goal(goal, feedback_cb=print_feedback)
+        while client.get_state() == 0 or client.get_state() == 1:
+            client.wait_for_result(rospy.Duration.from_sec(1.0))
+
+        result = client.get_result()
+        data_set = parse_measurement(result, goal.twist)
+        plot_and_save_data_set(goal.twist, data_set)
 
 
-    def parse_twist_list(self, twist):
-        goal = MoveAndRecordGoal()
-        goal.twist.postition.x = twist[0]
-        goal.twist.postition.y = twist[1]
-        goal.twist.postition.z = twist[2]
-        goal.twist.orientation.x = twist[3]
-        goal.twist.orientation.y = twist[4]
-        goal.twist.orientation.z = twist[5]
-        goal.target_duration = twist[6]
+def parse_twist_list(twist):
+    goal = MoveAndRecordGoal()
+    goal.twist.linear.x = twist[0]
+    goal.twist.linear.y = twist[1]
+    goal.twist.linear.z = twist[2]
+    goal.twist.angular.x = twist[3]
+    goal.twist.angular.y = twist[4]
+    goal.twist.angular.z = twist[5]
+    goal.target_duration = rospy.Duration.from_sec(twist[6])
+    
+    return goal
+
+
+def parse_measurement(record, twist):
+    data_set = {"time": [], "pos_x": [[], [], []], "pos_y": [[], [], []], "pos_z": [[], [], []], "rot_x": [[], [], []], "rot_y": [[], [], []], "rot_z": [[], [], []]}
+
+    time_last = rospy.Time.from_sec(0.0)
+    pos_x_last = 0.0
+    pos_y_last = 0.0
+    pos_z_last = 0.0
+    rot_x_last = 0.0
+    rot_y_last = 0.0
+    rot_z_last = 0.0
+
+    # parse time
+    for time in record.time:
+        data_set["time"].append(time.to_sec())
         
-        return goal
+
+    for pose in record.poses:
+        # parse poses
+        data_set["pos_x"][0].append(pose.position.x)
+        data_set["pos_y"][0].append(pose.position.y) 
+        data_set["pos_z"][0].append(pose.position.z) 
+
+        #KDL quaternion in r-p-y
+        temp_rot = PyKDL.Rotation.Quaternion(pose.orientation.x, pose.orientation.y,
+                                             pose.orientation.z, pose.orientation.w)
+        (roll, pitch, yaw) = temp_rot.GetRPY()
+        data_set["rot_x"][0].append(roll)
+        data_set["rot_y"][0].append(pitch) 
+        data_set["rot_z"][0].append(yaw)
+
+        # calc velocity
+        #dt = pose.header.stamp - time_last
+        #dt = dt.to_sec()
+        #time_last = pose.header.stamp
+        #data_set["pos_x"][1].append((pose.position.x - pos_x_last) / dt)
+        #data_set["pos_y"][1].append((pose.position.y - pos_x_last) / dt) 
+        #data_set["pos_z"][1].append((pose.position.z - pos_x_last) / dt) 
+
+        #data_set["rot_x"][1].append((pose.orientation.x - rot_x_last) / dt)
+        #data_set["rot_y"][1].append((pose.orientation.y - rot_x_last) / dt) 
+        #data_set["rot_z"][1].append((pose.orientation.z - rot_x_last) / dt)
+
+        # parse twist
+        data_set["pos_x"][2].append(twist.linear.x)
+        data_set["pos_y"][2].append(twist.linear.y) 
+        data_set["pos_z"][2].append(twist.linear.z) 
+
+        data_set["rot_x"][2].append(twist.angular.x)
+        data_set["rot_y"][2].append(twist.angular.y) 
+        data_set["rot_z"][2].append(twist.angular.z)
+
+    return data_set
 
 
-    def parse_measurement(self, record, twist)
-        data_set = {"time": [], "pos_x": [[], [], []], "pos_y": [[], [], []], "pos_z": [[], [], []], "rot_x": [[], [], []], "rot_y": [[], [], []], "rot_z": [[], [], []]}
+def plot_and_save_data_set(twist, data_set):
+    # set up date
+    today = datetime.date.today()
 
-        time_last = 0.0
-        pos_x_last = 0.0
-        pos_y_last = 0.0
-        pos_z_last = 0.0
-        rot_x_last = 0.0
-        rot_y_last = 0.0
-        rot_z_last = 0.0
+    # make directory
+    twist_string = "_".join([str(twist.linear.x),
+                             str(twist.linear.y),
+                             str(twist.linear.z),
+                             str(twist.angular.x),
+                             str(twist.angular.y),
+                             str(twist.angular.z)])
+    path = os.path.join(rootdir, "twist_"+twist_string+str(today))
+    os.mkdir(path)
 
+    time = data_set["time"]
 
-        for pose in record.poses:
-            # parse time
-            data_set["time"].append(pose.header.stamp)
-            
-            # parse poses
-            data_set["pos_x"][0].append(pose.pose.position.x)
-            data_set["pos_y"][0].append(pose.pose.position.y) 
-            data_set["pos_z"][0].append(pose.pose.position.z) 
+    # save data set
+    with open(os.path.join(path, "data_set"), 'w') as data_file:
+        data_file.write(str(data_set))
 
-            #KDL quaternion in r-p-y
-            temp_rot = PyKDL.Roration.Quaternion(pose.pose.orientation.x, pose.pose.orientation.y,
-                                                 pose.pose.orientation.z, pose.pose.orientation.w)
-            (roll, pitch, yaw) = temp_rot.GetRPY()
-            data_set["rot_x"][0].append(roll)
-            data_set["rot_y"][0].append(pitch) 
-            data_set["rot_z"][0].append(yaw)
-
-            # calc velocity
-            dt = header.stamp - time_last
-            time_last = header.stamp
-            data_set["pos_x"][1].append((pose.pose.position.x - pos_x_last) / dt)
-            data_set["pos_y"][1].append((pose.pose.position.y - pos_x_last) / dt) 
-            data_set["pos_z"][1].append((pose.pose.position.z - pos_x_last) / dt) 
-
-            data_set["rot_x"][1].append((pose.pose.orientation.x - rot_x_last) / dt)
-            data_set["rot_y"][1].append((pose.pose.orientation.y - rot_x_last) / dt) 
-            data_set["rot_z"][1].append((pose.pose.orientation.z - rot_x_last) / dt)
-
-            # parse twist
-            data_set["pos_x"][2].append(twist.linear.x)
-            data_set["pos_y"][2].append(twist.linear.y) 
-            data_set["pos_z"][2].append(twist.linear.z) 
-
-            data_set["rot_x"][2].append(twist.angular.x)
-            data_set["rot_y"][2].append(twist.angular.y) 
-            data_set["rot_z"][2].append(twist.angular.z)
-
-        return data_set
-
-
-    def plot_and_save_data_set(self, twist, data_set)
-        # set up date
-        today = datetime.date.today()
-
-        # make directory
-        twist_string = "_".join([str(twist.linear.x),
-                                 str(twist.linear.y),
-                                 str(twist.linear.z),
-                                 str(twist.angular.x),
-                                 str(twist.angular.y),
-                                 str(twist.angular.z)])
-        path = os.path.join(self_.rootdir, "twist_"+twist_string+str(today))
-        os.mkdir(path)
-
-        time = data_set["time"]
-
-        # save data set
-        with open(os.path.join(path, "data_set"), 'w') as data_file:
-            data_file.write(str(data_set))
-
-        # plot data
-        for name, data in data_set.iteritems():
-            if name != "time":
-                plt.plot(time, data[0], 'r', time, date[1], 'b--', time, data[2], 'g')
-                plt.savefig(os.path.join(path, str(today)+"_"+name), format="pdf")
+    # plot data
+    for name, data in data_set.iteritems():
+        if name != "time":
+            plt.plot(time, data[0])#, 'r-', time, data[2], 'g-')#, time, data[1], 'b--') 
+            plt.savefig(os.path.join(path, str(today)+"_"+name+".pdf"), format="pdf")
+            print "Plot %s saved"%name
+            plt.clf()
 
 
 
 
-    def print_feedback(self, feedback):
-        print "Feedback: " + feedback
+def print_feedback(feedback):
+    print "Feedback: ", feedback
 
 
 if __name__ == '__main__':
-    try:
-        rospy.init_node('moveAndRecord_client')
-        move_and_record_client()
-        rospy.spin()
-    except rospy.ROSInterruptException: pass
+    main()
