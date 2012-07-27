f404f06dfe30336d6689895309cbdd36245f1132
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2012-07-27 18:39:17 +0200

    update script

--- cob_manipulation_config/bin/update_planning_description.sh
+++ cob_manipulation_config/bin/update_planning_description.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $# -ne 1 ]
+if [ $# -lt 1 ]
 then
     echo "Usage: update_planning_description ROBOT"
     echo "It may take hours to finish!"
@@ -7,17 +7,27 @@ then
 elif [ "$1" == "ALL" ]
 then
     robots=""
-    for d in `rospack find cob_manipulation_config`/*/planning_description.yaml; do robots="$robots $(basename $(dirname $d))"; done    
+    for d in `rospack find cob_manipulation_config`/*/default_collision_operations.yaml; do robots="$robots $(basename $(dirname $d))"; done    
 else
     robots=$1
 fi
+if [ $# -eq 2 ]
+then
+    safety=$2
+else
+    safety=VerySafe
+fi
+
 trap "kill 0" SIGINT SIGTERM EXIT
 roscore&
 disown $!
 for r in $robots
 do
     echo $r
-    nice rosrun cob_arm_navigation planning_description_generator --input "$(rospack find cob_hardware_config)/$r/urdf/$r.urdf.xacro" --type VerySafe --output "$(rospack find cob_manipulation_config)/$r/planning_description.yaml"&
+    config=$(rospack find cob_manipulation_config)/$r/planning_description.yaml
+    urdf=$(rospack find cob_hardware_config)/$r/urdf/$r.urdf.xacro
+    output=$(rospack find cob_manipulation_config)/$r/default_collision_operations.yaml
+    nice rosrun cob_arm_navigation planning_description_generator --urdf "$urdf" --safety "$safety" --config "$config" --output "$output" --shrink&
 done
 wait
 
