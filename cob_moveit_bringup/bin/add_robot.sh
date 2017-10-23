8f214f7363e3525be4a4b278062eadc543a7bcda
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2017-10-23 12:52:42 +0200

    simplify template substitution

--- cob_moveit_bringup/bin/add_robot.sh
+++ cob_moveit_bringup/bin/add_robot.sh
@@ -10,17 +10,17 @@ echo "Adding moveit_config for "$robot" in "$pkg_moveit_config
 
 mkdir -p $pkg_moveit_config/robots/$robot/moveit/config
 
-cat <<SRDF_TEMPLATE  | sed "s/ROBOT/$robot/g" > $pkg_moveit_config/robots/$robot/moveit/config/$robot.srdf
-<robot name="ROBOT">
+cat <<SRDF_TEMPLATE > $pkg_moveit_config/robots/$robot/moveit/config/$robot.srdf
+<robot name="$robot">
 </robot>
 SRDF_TEMPLATE
 
-cat <<CONFIG_TEMPLATE  | sed "s/ROBOT/$robot/g;s/PKG_HARDWARE_CONFIG_NAME/$pkg_hardware_config_name/g" > $pkg_moveit_config/robots/$robot/moveit/.setup_assistant
+cat <<CONFIG_TEMPLATE > $pkg_moveit_config/robots/$robot/moveit/.setup_assistant
 moveit_setup_assistant_config:
   URDF:
-    package: PKG_HARDWARE_CONFIG_NAME
-    relative_path: robots/ROBOT/urdf/ROBOT.urdf.xacro
+    package: $pkg_hardware_config_name
+    relative_path: robots/$robot/urdf/$robot.urdf.xacro
     use_jade_xacro: true
   SRDF:
-    relative_path: config/ROBOT.srdf
+    relative_path: config/$robot.srdf
 CONFIG_TEMPLATE
